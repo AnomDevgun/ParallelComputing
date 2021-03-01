183d2acff6de4d06476fafc1d29857de41a945e5
AnomDevgun <rdevgun7@gmail.com>
2021-03-01 16:23:33 +0530

    OpenMP HelloWorld

--- openMP/HelloWorld.c
+++ openMP/HelloWorld.c
@@ -1,12 +1,18 @@
 //Have set OMP_NUM_THREADS = 8, therefore hello world will be printed 8 times as each thread carries out an individual task.
+//export OMP_NUM_THREADS=num_threads
+
 
 #include<stdio.h>
 #include<omp.h>
+
 int main(){
+	//What the following does is tell the system to give us the number of threads requested(in this case it will be what has been set as the default using the OMP_NUM_THREADS command)
+	//Structured block that binds with the parallel pragma.
 	#pragma omp parallel
 	{
 		int id = omp_get_thread_num();
-		printf("Hello%d",id);
-		printf("World%d\n",id);
+		printf("Hello");
+		printf("World	(%d)\n",id);
 	}
+	
 }
